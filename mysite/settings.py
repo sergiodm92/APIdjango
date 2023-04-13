3c8e41d57c545d08d6ddd2fc40539fd5c4f00e0c
sergiodm92 <crgiodm@gmail.com>
2023-04-13 20:05:37 -0300

    django app start

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # DATABASE_URL
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
